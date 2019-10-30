bd974a94b9ca194e82c0e6b29e0831942b2d8a11
Jeremy Rogel <jeremyrogel@Jeremys-MBP.lan1>
2019-10-30 17:43:49 -0400

    mvp

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-// code away!
+const server = require('./server');
+
+server.listen(5000, () => console.log("server on port 5000"));
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-const express = 'express';
+  
+const express =  require('express');
 
 const server = express();
+
+server.use(express.json());
+server.use (logger);
+
+const userRouter = require('./users/userRouter.js');
+server.use('/api/users', userRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
@@ -9,7 +16,8 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
-
+  console.log(req.method, req.url, Date.now());
+  next();
 };
 
 module.exports = server;
